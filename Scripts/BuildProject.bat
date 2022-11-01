5db008facc8a565320db9a6a22721199828f2473
Gevorg Adamyan <gevorg@wolf3d.io>
2022-11-01 22:05:51 +0100

    update

--- Scripts/BuildProject.bat
+++ Scripts/BuildProject.bat
@@ -21,7 +21,7 @@ powershell -Command  "Remove-Item -LiteralPath %ProjectDir%..\Builds\ -Force -Re
 powershell -Command  "Remove-Item -LiteralPath %ProjectDir%..\Archive\ -Force -Recurse -ErrorAction SilentlyContinue"
 
 rem -rocket -prereqs -skipbuildeditor
-%EngineDir%Build\BatchFiles\RunUAT.bat BuildCookRun -project=%ProjectDir%UE5ThirdPersonEx.uproject -noP4 -clientconfig=Development -serverconfig=Development -unrealexe=%EngineDir%Binaries\Win64\UnrealEditor-Cmd.exe -utf8output -platform=Win64 -targetplatform=Win64 -build -cook -platform=Win64 -unversionedcookedcontent -skipcookingeditorcontent -unversionedcookedcontent -pak -distribution -compressed -stage -package -stagingdirectory=%ProjectDir%..\Builds -archive -archivedirectory=%ProjectDir%..\Archive
+%EngineDir%Build\BatchFiles\RunUAT.bat BuildCookRun -project=%ProjectDir%UE5ThirdPersonEx.uproject -noP4 -clientconfig=Development -serverconfig=Development -unrealexe=%EngineDir%Binaries\Win64\UnrealEditor-Cmd.exe -utf8output -platform=Win64 -targetplatform=Win64 -build -cook -platform=Win64 -unversionedcookedcontent -skipcookingeditorcontent -pak -distribution -compressed -stage -package -map=ThirdPersonMap+ThirdPersonMap -stagingdirectory=%ProjectDir%..\Builds -archive -archivedirectory=%ProjectDir%..\Archive
 
 powershell -Command "Compress-Archive -Path %ProjectDir%..\Archive\ -DestinationPath %ProjectDir%..\Archive.zip"
 
